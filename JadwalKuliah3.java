ed1fe413108299d0e32fee6683b9d7c8ea136df7
Shellahariza <150937297+Shellahariza@users.noreply.github.com>
2023-11-30 08:18:53 +0700

    create by s

---
+++ JadwalKuliah3.java
@@ -0,0 +1,7 @@
+public class JadwalKuliah3 extends JadwalKuliah {
+    public JadwalKuliah3(MataKuliah mataKuliah, String hari, String jamMulai, String jamSelesai) {
+        super(mataKuliah, hari, jamMulai, jamSelesai);
+    }
+
+    // Tambahan atribut atau metode khusus untuk JadwalKuliah3
+}
