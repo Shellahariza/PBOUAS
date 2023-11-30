9c87f82e38b8ad12433e5f5d9a82e404d882db28
Shellahariza <150937297+Shellahariza@users.noreply.github.com>
2023-11-30 07:26:32 +0700

    edit by shella

--- JadwalKuliah.java
+++ JadwalKuliah.java
@@ -1,23 +1,27 @@
 class JadwalKuliah {
     private MataKuliah mataKuliah;
     private String hari;
-    private String jam;
+    private String jamMulai;
+    private String jamSelesai;
 
-    public JadwalKuliah(MataKuliah mataKuliah, String hari, String jam) {
+    public JadwalKuliah(MataKuliah mataKuliah, String hari, String jamMulai, String jamSelesai) {
         this.mataKuliah = mataKuliah;
         this.hari = hari;
-        this.jam = jam;
+        this.jamMulai = jamMulai;
+        this.jamSelesai = jamSelesai;
     }
 
     public boolean bentrok(JadwalKuliah jadwalLain) {
-        // Mengubah hari menjadi huruf kecil sebelum membandingkan
-        String hariLower = this.hari.toLowerCase();
-        String hariLainLower = jadwalLain.hari.toLowerCase();
-
-        return hariLower.equals(hariLainLower) && this.jam.equals(jadwalLain.jam);
+        // Logika cek bentrok jadwal
+        // Misalnya, jika hari sama dan rentang jam saling bersinggungan
+        return this.hari.equalsIgnoreCase(jadwalLain.hari) &&
+               (this.jamMulai.compareTo(jadwalLain.jamSelesai) < 0) &&
+               (this.jamSelesai.compareTo(jadwalLain.jamMulai) > 0);
     }
 
     public void tampilkanJadwal() {
-        System.out.println("Mata Kuliah: " + mataKuliah.getNamaMataKuliah() + "\nHari: " + hari + "\nJam: " + jam);
+        System.out.println("Mata Kuliah: " + mataKuliah.getNamaMataKuliah() +
+                           "\nHari: " + hari +
+                           "\nJam: " + jamMulai + " - " + jamSelesai);
     }
 }
